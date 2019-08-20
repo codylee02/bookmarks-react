9814bb4d0f5934d9f386f8f2ac7779cef7b81124
Cody Lee <codylee02@gmail.com>
2019-08-20 11:19:46 -0600

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import AddBookmark from './addBookmark/AddBookmark';
+import BookmarkApp from './bookmarkApp/BookmarkApp';
+
+
+// const bookmarks = [
+//   {
+//     title:"Google",
+//     url: "http://www.google.com",
+//     rating: "3",
+//     description: "No evil"
+//   },
+//     {
+//       title:"Facebook",
+//       url: "http://www.facebook.com",
+//       rating: "5",
+//       description: "Spend too much time here"
+//     }
+// ]
+
+
+
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: [],
+      showAddForm: false
+    }
+  }
+
+  setShowAddForm(show) {
+    this.setState({
+      showAddForm: show
+    })
+  }
+
+  addBookmark(bookmark) {
+    this.setState({
+      bookmarks: [...this.state.bookmarks, bookmark],
+      showAddForm: false
+    });
+  }
+
+  componentDidMount() {
+    const url = 'https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks';
+    const options = {
+      method: 'GET',
+      headers: {
+        "Authorization": "Bearer $2a$10$Q6FyXSZBsFXyqgD93Ab0euXmeGfE3hm.jdBjwUVlSDDDU.DwGNmt2",
+        "Content-Type": "application/json"
+      }
+    };
+
+    fetch(url, options)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong, please try again later');
+        }
+        return res;
+      })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          bookmarks: data,
+          error: null
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+  }
+
+  render() {
+    const page = this.state.showAddForm
+      ? <AddBookmark 
+        showForm={show => this.setShowAddForm(show)}
+        handleAdd={bookmark => this.addBookmark(bookmark)} />
+      : <BookmarkApp 
+        bookmarks={this.state.bookmarks} 
+        showForm={show => this.setShowAddForm(show)} />
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+      {page}
+    </main>
   );
 }
-
-export default App;
+}
---
+++ src/fab/fab.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './fab.css';
+
+export default class Fab extends React.Component {
+    render() {
+        return (
+            <div className="fab" onClick={e => this.props.showForm(true)} >
+                &#43;
+            </div>
+        );
+    }
+}

fc71ae89f612953308f337106a50654039dc26ca
Cody Lee <codylee02@gmail.com>
2019-08-25 20:44:05 -0600

    completed assignment

--- src/bookmarkList/bookmarkList.js
+++ src/bookmarkList/bookmarkList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import BookmarksContext from '../BookmarksContext';
 import BookmarkItem from '../BookmarkItem/BookmarkItem';
 import './BookmarkList.css'
+import PropTypes from 'prop-types';
 
 class BookmarkList extends Component {
   static contextType = BookmarksContext;
@@ -23,5 +24,9 @@ class BookmarkList extends Component {
     );
   }
 }
+
+BookmarkList.propTypes = {
+  bookmarks: PropTypes.arrayOf(PropTypes.object)
+};
 
 export default BookmarkList;
